ccaaa2f0c4866b0f5a3e88c04816dc561f49c411
Kenneth Dahlstrøm <kenneth@keda.no>
2016-08-19 13:11:34 +0200

    Added comments and cleaned up some console.logs
    Signed-off-by: Kenneth Dahlstrøm <kenneth@keda.no>

--- app/assets/js/components/_component_library.js
+++ app/assets/js/components/_component_library.js
@@ -59,26 +59,19 @@ class LibraryView {
       {},
       this.defaults
     );
-    //console.log(this.i18n);
     //this.view.i18n.success(function(data){
     //  this.i18n = data;
     //});
     let indexRender = $.Event("indexrender");
     let that = this;
     this.view.getData(settings, that).success(function(data){
-      //data.meta = settings;
-      //console.log('Settings from renderLibrary');
-      //console.log(settings);
-      //console.log('Data from renderLibrary');
       data.settings = settings;
-      //console.log(that.pageFooterData);
       // Populate templates with data
       let header = that.headerTemplate(data),
       breadcrumb = that.breadcrumbTemplate(data),
       navbar = that.navbarTemplate(data),
       content = that.contentTemplate(data),
       footer = that.pageFooterTemplate(that.pageFooterData);
-      //console.log(footer);
 
       $('.library-portal').html(header + navbar + content + footer + breadcrumb);
       $('.library-navigation').affix({
@@ -111,7 +104,6 @@ class LibraryView {
       this.defaults
     );
     data.settings = settings;
-    //console.log(data);
     let articleRender = $.Event("articlerender");
     let boxRender = $.Event("boxrender");
     let information = this.informationTemplate(),
@@ -151,7 +143,6 @@ class LibraryView {
 
   renderPages(options = {}) {
     let that = this;
-    //console.log(that);
 
     let settings = Object.assign(
       {},
@@ -168,10 +159,8 @@ class LibraryView {
 
         data.page[0].specialfunction = accordionView;
       }
-      //console.log(data);
       let markup = this.pageShowTemplate(data);
       let breadcrumb = this.breadcrumbTemplate(data);
-      //console.log(markup);
       $('.library-content').html(markup).addClass('lo-auron-2-3');
       $('.library-breadcrumb').html(breadcrumb);
       that.view.scrollToElement('.library-portal');
@@ -236,13 +225,11 @@ class LibraryView {
 
       });
     };
-
+    // Update google analytics on page-navigation
     updateAnalytics(){
-      //console.log('analaytics updated...');
       // Send page update to hiof.no analytics
       ga('set', 'page', document.location.href);
       ga('send', 'pageview');
-
       // Send page update to library analytics
       ga('library.set', 'page', document.location.href);
       ga('library.send', 'pageview');
@@ -342,13 +329,11 @@ class LibraryView {
 
 
       Path.map("#/portal").enter(function() {
-        //console.log('page entered..');
         library.updateAnalytics();
       }).to(function() {
         library.renderLibrary();
       });
       Path.map("#/portal/").enter(function() {
-        //console.log('page entered..');
         library.updateAnalytics();
       }).to(function() {
         library.renderLibrary();
@@ -356,7 +341,6 @@ class LibraryView {
 
 
       Path.map("#/:articletitle/a/:articleid").enter(function() {
-        //console.log('page entered..');
         library.updateAnalytics();
       }).to(function() {
         var opt = {};
@@ -384,7 +368,6 @@ class LibraryView {
         library.renderLibrary(opt);
       });
       Path.map("#/:pagetitle/:pagetitle2").enter(function() {
-        //console.log('page entered..');
         library.updateAnalytics();
       }).to(function() {
         var opt = {};
